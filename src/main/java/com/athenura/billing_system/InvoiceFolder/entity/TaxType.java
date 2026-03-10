03db692eb2ad5d6d4ea5ba91266a2b5568c0add1
Lokesh Sijapati <lokeshkr189@gmail.com>
2026-03-10 12:46:10 +0530

    GSTP

---
+++ src/main/java/com/athenura/billing_system/InvoiceFolder/entity/TaxType.java
@@ -0,0 +1,8 @@
+package com.athenura.billing_system.InvoiceFolder.entity;
+
+public enum TaxType {
+
+    CGST_SGST,
+    IGST,
+    NONE
+}
--- src/main/java/com/athenura/billing_system/InvoiceFolder/serviceImpl/AsyncInvoiceProcessor.java
+++ src/main/java/com/athenura/billing_system/InvoiceFolder/serviceImpl/AsyncInvoiceProcessor.java
@@ -1,13 +1,17 @@
 package com.athenura.billing_system.InvoiceFolder.serviceImpl;
+
 import com.athenura.billing_system.InvoiceFolder.dto.InvoiceResponseDTO;
 import com.athenura.billing_system.InvoiceFolder.entity.Invoice;
 import com.athenura.billing_system.InvoiceFolder.entity.InvoiceStatus;
 import com.athenura.billing_system.InvoiceFolder.mapper.InvoiceMapper;
 import com.athenura.billing_system.InvoiceFolder.repository.InvoiceRepository;
 import com.athenura.billing_system.client.entity.Client;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,36 +21,57 @@ public class AsyncInvoiceProcessor {
     private final EmailService emailService;
     private final CloudinaryService cloudinaryService;
     private final InvoiceRepository invoiceRepository;
+@Async
+@Transactional
+public void processInvoice(Long invoiceId) {
 
-    @Async
-    public void processInvoice(Invoice invoice, Client client) {
+    Invoice invoice = null;
 
-        try {
+    try {
 
-            InvoiceResponseDTO dto = InvoiceMapper.toDTO(invoice);
+        System.out.println("STEP 1 : Fetch invoice");
 
-            // 1 PDF generate
-            byte[] pdfBytes = invoicePdfService.generateInvoicePdf(dto);
+        invoice = invoiceRepository.findById(invoiceId)
+                .orElseThrow(() -> new RuntimeException("Invoice not found"));
 
-            // 2 upload cloudinary
-            String pdfUrl = cloudinaryService.uploadPdf(pdfBytes, invoice.getInvoiceNumber());
-            invoice.setPdfUrl(pdfUrl);
+        Client client = invoice.getClient();
 
-            // 3 send email
-            emailService.sendInvoiceEmail(
-                    client.getEmail(),
-                    pdfBytes,
-                    invoice.getInvoiceNumber() + ".pdf"
-            );
+        System.out.println("STEP 2 : Convert DTO");
 
-            invoice.setStatus(InvoiceStatus.SENT);
+        InvoiceResponseDTO dto = InvoiceMapper.toDTO(invoice);
 
-        } catch (Exception e) {
+        System.out.println("STEP 3 : Generate PDF");
 
-            invoice.setStatus(InvoiceStatus.FAILED);
+        byte[] pdfBytes = invoicePdfService.generateInvoicePdf(dto);
 
-        }
+        System.out.println("STEP 4 : Upload Cloudinary");
+
+        String pdfUrl = cloudinaryService.uploadPdf(pdfBytes, invoice.getInvoiceNumber());
+
+        invoice.setPdfUrl(pdfUrl);
+
+        System.out.println("STEP 5 : Send Email");
+
+        emailService.sendInvoiceEmail(
+                client.getEmail(),
+                pdfBytes,
+                invoice.getInvoiceNumber() + ".pdf"
+        );
+
+        invoice.setStatus(InvoiceStatus.SENT);
 
         invoiceRepository.save(invoice);
+
+        System.out.println("ASYNC SUCCESS");
+
+    } catch (Exception e) {
+
+        e.printStackTrace();
+
+        if (invoice != null) {
+            invoice.setStatus(InvoiceStatus.FAILED);
+            invoiceRepository.save(invoice);
+        }
     }
+}
 }
